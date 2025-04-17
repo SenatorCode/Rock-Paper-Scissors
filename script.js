bf840bcc39b0917ad81cd17823f2ef6f6fa2c6ee
Senator's Code <folamideibikunle@gmail.com>
2025-04-17 17:16:55 +0100

    Fixed the bugs in the playRound function

--- script.js
+++ script.js
@@ -25,27 +25,62 @@ function getHumanChoice () {
 // console.log(getHumanChoice())
 
 function playGame (){
+    // let humanScore = 0;
+    // let computerScore = 0;
+    // function playRound (computerChoice, humanChoice) {
+    //     if (computerChoice === "rock" && humanChoice === "paper") {
+    //         alert("Paper beats rock, therefore you win this round Hurayyyy!!!");
+    //         humanScore++;
+    //     } else if (computerChoice === "paper" && humanChoice === "rock") {
+    //         alert("Paper beats rock, therefore you lose this round");
+    //         computerScore++;
+    //     } else if (computerChoice === "paper" && humanChoice === "scissors") {
+    //         alert("Scissors cut through paper, therefore you win this round Hurayyyy!!!");
+    //         humanChoice++;
+    //     } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    //         alert("Scissors cut through paper, therefore you lose this round");
+    //         computerChoice++;
+    //     } else if (computerChoice === "rock" && humanChoice === "scissors") {
+    //         alert("Scissors is harmless against rock, therefore you lose this round")
+    //         computerChoice++;
+    //     } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    //         alert("Scissors is harmless against rock, therefore you win this round, Hurayyyy!!!");
+    //         humanChoice++;
+    //     } else if (computerChoice === humanChoice){
+    //         alert("No one win this round, which means it is a tie");
+    //     } else {
+    //         alert("Wrong entry")
+    //         getHumanChoice();
+    //     };
+    // }
+    // const humanSelection = getHumanChoice();
+    // const computerSelection = getComputerChoice();
+    // playRound (humanSelection, computerSelection);
+
     let humanScore = 0;
-    let computerScore = 0;
+    let computerScore= 0;
+
     function playRound (computerChoice, humanChoice) {
+        let playerScore;
+        let compScore;
         if (computerChoice === "rock" && humanChoice === "paper") {
             alert("Paper beats rock, therefore you win this round Hurayyyy!!!");
-            humanScore++;
+            return ++playerScore;
         } else if (computerChoice === "paper" && humanChoice === "rock") {
             alert("Paper beats rock, therefore you lose this round");
-            computerScore++;
+            return ++compScore;
         } else if (computerChoice === "paper" && humanChoice === "scissors") {
             alert("Scissors cut through paper, therefore you win this round Hurayyyy!!!");
-            humanChoice++;
+            return ++playerScore;
         } else if (computerChoice === "scissors" && humanChoice === "paper") {
             alert("Scissors cut through paper, therefore you lose this round");
-            computerChoice++;
+            return ++compScore;
         } else if (computerChoice === "rock" && humanChoice === "scissors") {
             alert("Scissors is harmless against rock, therefore you lose this round")
-            computerChoice++;
+            return ++compScore;
         } else if (computerChoice === "scissors" && humanChoice === "rock") {
             alert("Scissors is harmless against rock, therefore you win this round, Hurayyyy!!!");
-            humanChoice++;
+            return ++playerScore;
         } else if (computerChoice === humanChoice){
             alert("No one win this round, which means it is a tie");
         } else {
@@ -53,8 +88,9 @@ function playGame (){
             getHumanChoice();
         };
     }
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound (humanSelection, computerSelection);
-    
+    const humanSelection = getHumanChoice().toLowerCase();
+    const computerSelection = getComputerChoice().toLowerCase();
+    playRound(humanSelection, computerSelection);
 }
+
+playGame();
